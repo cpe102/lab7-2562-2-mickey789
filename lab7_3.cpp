2e128b94b12e5b1ba40fefa79e8e54f40ee06eb7
lab302-39 <lab302-39@labeng.local>
2020-01-21 15:52:48 +0700

    v1

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,42 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A, T, M;
+    string status;
+    cout << "Age : ";
+    cin >> A;
+    if(A <= 20){
+        cout << "How Tall : ";
+        cin >> T;
+        if(T < 160){
+            status = "UNFRIEND";
+        }else if(T < 175){
+            status = "FRIEND";
+        }else{
+            cout << "Asset : ";
+            cin >> M;
+            if(M > 69000000){
+                status = "MARRIED";
+            }else{
+                status = "ONE-NIGHT-STAND";
+            }
+        }
+    }else if(A < 30){
+        cout << "Asset : ";
+        cin >> M;
+        if(M > 10000000){
+            status = "BEST FRIEND";
+        }else{
+            status = "UNFRIEND";
+        }
+    }else{
+        status = "UNFRIEND";
+    }
+    
+    cout << status;
+
+    return 0;
+
+}
